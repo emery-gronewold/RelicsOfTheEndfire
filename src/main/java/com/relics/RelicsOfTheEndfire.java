82fa8da0a030da3fd90ce6b7c30e5c440cb8e3bd
emery-gronewold <emery.gronewold@outlook.com>
2025-06-18 23:00:42 -0500

    Update RelicsOfTheEndfire.java

--- src/main/java/com/relics/RelicsOfTheEndfire.java
+++ src/main/java/com/relics/RelicsOfTheEndfire.java
@@ -1,11 +1,9 @@
 package com.relics;
 
-import com.relics.registry.ItemInit;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.relics.registry.ItemInit;
 
 @Mod(RelicsOfTheEndfire.MODID)
 public class RelicsOfTheEndfire {
@@ -13,22 +11,6 @@ public class RelicsOfTheEndfire {
 
     public RelicsOfTheEndfire() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         ItemInit.ITEMS.register(modEventBus);
-
-        modEventBus.addListener(this::addCreative);
-    }
-
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTabKey().location().toString().equals("minecraft:combat")) {
-            event.accept(ItemInit.FLAMING_RUBY_AXE.get());
-            event.accept(ItemInit.GLOWING_AMBER_SWORD.get());
-            event.accept(ItemInit.INFERNAL_THORNBLADE.get());
-            event.accept(ItemInit.TOTEM_OF_MALICE.get());
-        }
-    }
-
-    public static ResourceLocation id(String name) {
-        return new ResourceLocation(MODID, name);
     }
 }
