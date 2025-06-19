dcb30b52bc6d2396831cac2247adabea45256c7c
emery-gronewold <emery.gronewold@outlook.com>
2025-06-18 23:02:02 -0500

    Update FlamingRubyAxe.java

--- src/main/java/com/relics/items/FlamingRubyAxe.java
+++ src/main/java/com/relics/items/FlamingRubyAxe.java
@@ -1,17 +1,15 @@
 package com.relics.items;
 
 import net.minecraft.world.item.AxeItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
-import net.minecraft.world.item.Item.Properties;
-import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 
 public class FlamingRubyAxe extends AxeItem {
     public FlamingRubyAxe() {
-        super(Tiers.NETHERITE, 10.0F, -3.0F, new Properties().durability(1200).fireResistant());
+        super(Tiers.NETHERITE, 10.0F, -3.0F, new Item.Properties().durability(1200).fireResistant());
     }
 
     @Override
